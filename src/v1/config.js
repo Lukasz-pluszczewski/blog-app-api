161fa7400c9075c2c96f18044fb02b9b93069ec6
lukasz_brainhub <lukasz@brainhub.pl>
2018-01-19 20:43:07 +0100

    Fixed issue with config endpoint

--- src/v1/config.js
+++ src/v1/config.js
@@ -6,7 +6,7 @@ import AuthenticationMiddleware from 'middleware/AuthenticationMiddleware';
 export default ({ RequireAuthentication }) => {
   const router = new Router();
   return router
-    .get('/', AuthenticationMiddleware.authenticate, (req, res) => {
+    .get('/', (req, res) => {
       const db = getDb();
       const config = db.get('config').value();
       return res.status(200).json(config || {});
